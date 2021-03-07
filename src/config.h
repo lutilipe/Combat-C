609e486f81435a0347e13f4de76cb457802590ba
lutilipe <lutilipe02@hotmail.com>
2021-03-07 08:59:34 -0300

    refactor: change config.h

--- src/config.h
+++ src/config.h
@@ -1,35 +1,18 @@
-#ifndef FPS
+#ifndef CONFIG_H
+#define CONFIG_H
+
 #define FPS 60  
-#endif
 
-#ifndef SCREEN_W
 #define SCREEN_W 960
-#endif
-
-#ifndef SCREEN_H
 #define SCREEN_H 540
-#endif
 
-#ifndef THETA
 #define THETA M_PI/4
-#endif
 
-#ifndef RADIUS_FORCE_FIELD
 #define RADIUS_FORCE_FIELD 30
-#endif
-
-#ifndef RADIUS_SHOT
 #define RADIUS_SHOT 6.0
-#endif
 
-#ifndef TANK_SPEED
 #define TANK_SPEED 2.5
-#endif
-
-#ifndef TANK_SHOT_SPEED
 #define TANK_SHOT_SPEED 10
-#endif
-
-#ifndef TANK_ANGULAR_SPEED
 #define TANK_ANGULAR_SPEED M_PI/90
+
 #endif
